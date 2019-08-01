d4d7300a45da82429a9cdd35d7e5e6ce20932efd
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-08-01 11:32:12 +0200

    Fix link interface

--- src/Link/Link.interface.ts
+++ src/Link/Link.interface.ts
@@ -1,9 +1,10 @@
 import * as React from 'react'
 
-import { styledAs } from '../_internal/types'
+import { styledAs, ThemeOverridesProps } from '../_internal/types'
 
 export default interface LinkProps
-  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    ThemeOverridesProps {
   newTab?: boolean
   as?: styledAs
 }
