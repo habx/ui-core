d973cf4f58815289db1c5fe5c8dde0f4af2e28d8
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-10-29 10:31:41 +0100

    APP-7118 lot of http error 500

--- src/_internal/ssr.ts
+++ src/_internal/ssr.ts
@@ -6,11 +6,16 @@ export const useSSRLayoutEffect = isClientSide
   ? React.useLayoutEffect
   : React.useEffect
 
-export const ssrClientRect: ClientRect = {
+const SSR_DOM_RECT: DOMRect = {
   height: 0,
   width: 0,
   top: 0,
   left: 0,
   right: 0,
   bottom: 0,
+  x: 0,
+  y: 0,
+  toJSON: () => '',
 }
+
+export const getDOMRect = () => (isClientSide ? new DOMRect() : SSR_DOM_RECT)
