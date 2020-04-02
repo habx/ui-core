b877a1644fabf766f3f705d925a77fb8d0d70472
Jean Dessane <jean@habx.fr>
2020-04-02 14:15:26 +0200

    fix css var

--- src/Provider/CssVariables.ts
+++ src/Provider/CssVariables.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  * {
+  html {
     --layout-right-padding: 24px;
     --layout-left-padding: 24px;
     --expansion-panel-horizontal-padding: 24px;
