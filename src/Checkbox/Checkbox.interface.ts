088fa0ef4749a69855cbdb977724df9420d2d9f2
Alexis Fontaine <alexis.fontaine@outlook.com>
2020-08-25 23:17:55 +0200

    Disallow switching the `<input>` element with another The implementation relies on native capabilities of `<input type="checkbox">`, specifically the `checked` attribute which is not available on other HTML elements.

--- src/Checkbox/Checkbox.interface.ts
+++ src/Checkbox/Checkbox.interface.ts
@@ -1,13 +1,11 @@
 import * as React from 'react'
 
-import { styledAs } from '../_internal/types'
 import WithLabel from '../withLabel/withLabel.interface'
 
 export interface CheckboxInnerProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: boolean
   small?: boolean
-  as?: styledAs
 }
 
 export default interface CheckboxProps extends WithLabel<CheckboxInnerProps> {}
