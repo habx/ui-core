c2b66e9fb91a57e7261d2cd55d39b06d34984626
Flavien DELANGLE <flaviendelangle@gmail.com>
2020-12-04 10:24:23 +0100

    Missing exports for theme converter

--- typings/color-parse.d.ts
+++ typings/color-parse.d.ts
@@ -1,13 +1,5 @@
 declare module 'color-parse' {
-  export type ParsedColorValue = [number, number, number]
-
-  export type ParsedColor = {
-    space: 'rgb'
-    values: ParsedColorValue
-    alpha: number
-  }
-
-  const Module: (color: string) => ParsedColor
+  const Module: (color: string) => any
 
   export default Module
 }
