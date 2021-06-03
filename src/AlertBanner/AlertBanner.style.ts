a7bece8b4de5b861955a94e49922f909369d5761
CarolineVP <caroline_pujebet@hotmail.com>
2021-06-03 12:13:47 +0200

    fix remove z-index in `AlertBannerContent`

--- src/AlertBanner/AlertBanner.style.ts
+++ src/AlertBanner/AlertBanner.style.ts
@@ -8,7 +8,6 @@ export const AlertBannerContent = styled.div`
   justify-content: center;
   align-items: center;
   flex: 1 1 100%;
-  z-index: 1;
   padding: 12px 24px;
 
   @media (${breakpoints.below.smallTablet}) {
