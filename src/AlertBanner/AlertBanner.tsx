079f718f56e5c328a91cfb74ab5307ce442dd57c
CarolineVP <caroline_pujebet@hotmail.com>
2021-12-06 12:33:59 +0100

    update `AlertBanner`

--- src/AlertBanner/AlertBanner.tsx
+++ src/AlertBanner/AlertBanner.tsx
@@ -2,24 +2,23 @@ import * as React from 'react'
 
 import { Banner } from '../_internal/Banner'
 import { IconButton } from '../IconButton'
-import { useThemeVariant } from '../useThemeVariant'
 
 import { AlertBannerProps } from './AlertBanner.interface'
 import { AlertBannerContent, AlertBannerText } from './AlertBanner.style'
 
 export const AlertBanner = React.forwardRef<HTMLDivElement, AlertBannerProps>(
   (props, ref) => {
-    const { warning, message, onClose, ...rest } = props
-
-    const theme = useThemeVariant()
-
-    const backgroundColor = warning
-      ? theme.colors.warning.base
-      : theme.colors.primary.calm
+    const { message, warning, primary, success, error, onClose, ...rest } =
+      props
 
     return (
-      <Banner ref={ref} backgroundColor={backgroundColor} {...rest}>
-        <AlertBannerContent>
+      <Banner ref={ref} {...rest}>
+        <AlertBannerContent
+          data-warning={warning}
+          data-error={error}
+          data-success={success}
+          data-primary={primary}
+        >
           <AlertBannerText variation="title">{message}</AlertBannerText>
           {onClose && <IconButton icon="close" tiny onClick={onClose} />}
         </AlertBannerContent>
